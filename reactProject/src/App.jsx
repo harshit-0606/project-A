8e6569054946f2c1ef7c5d010c796ed5c06a8def
harshit-0606 <sharma.harshit0606@gmail.com>
2025-01-06 14:39:45 +0530

    background color change

---
+++ reactProject/src/App.jsx
@@ -0,0 +1,16 @@
+
+// import { useState } from 'react'
+
+ import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+  <p className='bg-blue-500 '>hello</p>
+    </>
+  )
+}
+
+export default App
