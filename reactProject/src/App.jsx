ae14937d083c1c2ad9d8138e7c3e6baf750eaaaf
harshit-0606 <sharma.harshit0606@gmail.com>
2025-01-06 14:42:47 +0530

    add useEffect hook

--- reactProject/src/App.jsx
+++ reactProject/src/App.jsx
@@ -1,16 +1,69 @@
+import { useState , useEffect } from "react";
 
-// import { useState } from 'react'
-
- import './App.css'
-
+import "./App.css";
 
 function App() {
+  const backgroundColor = localStorage.getItem("color") || "white";
+  const [color, setColor] = useState(backgroundColor);
 
+  useEffect(()=>{
+    localStorage.setItem("color",color)},[color]
+  )
   return (
     <>
-  <p className='bg-blue-500 '>hello</p>
+       {/* Full screen div with dynamic background color */}
+     <div className="fixed w-full h-screen" style={{ backgroundColor: color }}>
+  <h1 className="text-white text-3xl text-center">Background Color: {color}</h1>
+</div>
+
+       
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white rounded-lg shadow-lg">
+          <button
+            onClick={() => setColor("black")}
+            className="outline-none bg-black text-white px-2 py-2 mx-2 my-2 rounded-lg shadow-lg"
+          >
+            Click Me
+          </button>
+
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none bg-red-500 text-white px-2 py-2 mx-2 my-2 rounded-lg shadow-lg"
+          >
+            Click Me
+          </button>
+
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none bg-blue-500 text-white px-2 py-2 mx-2 my-2 rounded-lg shadow-lg"
+          >
+            Click Me
+          </button>
+
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none bg-green-500 text-white px-2 py-2 mx-2 my-2 rounded-lg shadow-lg"
+          >
+            Click Me
+          </button>
+
+          <button
+            onClick={() => setColor("orange")}
+            className="outline-none bg-orange-500 text-white px-2 py-2 mx-2 my-2 rounded-lg shadow-lg"
+          >
+            Click Me
+          </button>
+
+          <button
+            onClick={() => setColor("pink")}
+            className="outline-none bg-pink-500 text-white px-2 py-2 mx-2 my-2 rounded-lg shadow-lg"
+          >
+            Click Me
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
